371141e28d40cbdbdd4462acc1aaf7ed024efbea
sumayah AlGhafis <sumayah.alghafis@gmail.com>
2026-09-17 16:00:11 +0000

    Generate dummy hospital data

--- generate_data.py
+++ generate_data.py
@@ -19,7 +19,6 @@ department_info = {
 }
 
 num_patients = 200
-
 patients = []
 
 for i in range(1, num_patients + 1):
@@ -32,27 +31,34 @@ for i in range(1, num_patients + 1):
 
 visits = []
 visit_id = 1
-for patient in patients: num_visits = random.randint(1, 3)
-  for _ in range(num_visits):
-    department = random.choice(departments)
 
-    visit = {
-        "Visit_ID": visit_id,
-        "Patient_ID": patient["Patient_ID"],
-        "Department": department
-    }
+for patient in patients:
+    num_visits = random.randint(1, 3)
 
-    visits.append(visit)
-    visit_id += 1
+    for _ in range(num_visits):
+        department = random.choice(departments)
+
+        visit = {
+            "Visit_ID": visit_id,
+            "Patient_ID": patient["Patient_ID"],
+            "Department": department
+        }
+
+        visits.append(visit)
+        visit_id += 1
 
 start_date = datetime(2026, 1, 1)
-for visit in visits: random_days = random.randint(0, 364) arrival_date = start_date + timedelta(days=random_days)
-hour = random.randint(8, 20)
-minute = random.randint(0, 59)
 
-visit["Arrival_Date"] = arrival_date.strftime("%Y-%m-%d")
-visit["Arrival_Time"] = f"{hour:02d}:{minute:02d}"
-visit["Waiting_Time_Minutes"] = random.randint(5, 60)
+for visit in visits:
+    random_days = random.randint(0, 364)
+    arrival_date = start_date + timedelta(days=random_days)
+
+    hour = random.randint(8, 20)
+    minute = random.randint(0, 59)
+
+    visit["Arrival_Date"] = arrival_date.strftime("%Y-%m-%d")
+    visit["Arrival_Time"] = f"{hour:02d}:{minute:02d}"
+    visit["Waiting_Time_Minutes"] = random.randint(5, 60)
 
 with open("data/visits.csv", "w", newline="") as file:
     writer = csv.DictWriter(file, fieldnames=visits[0].keys())
@@ -64,10 +70,13 @@ with open("data/patients.csv", "w", newline="") as file:
     writer.writeheader()
     writer.writerows(patients)
 
-with open("data/departments.csv", "w", newline="") as file: writer = csv.writer(file) writer.writerow(["Department", "Staff_Count", "Resource_Count"])
-for department in departments:
-    writer.writerow([
-        department,
-        department_info[department]["staff"],
-        department_info[department]["resources"]
-    ])
+with open("data/departments.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Department", "Staff_Count", "Resource_Count"])
+
+    for department in departments:
+        writer.writerow([
+            department,
+            department_info[department]["staff"],
+            department_info[department]["resources"]
+        ])
